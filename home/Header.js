4afe6162bae23eb9e98e87a4c945335dfe65c1e2
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2019-10-30 11:21:23 -0700

    added code

---
+++ home/Header.js
@@ -0,0 +1,14 @@
+import Component from '../Component.js';
+
+class Header extends Component {
+    renderHTML() {
+        return /*html*/`
+            <header>
+            <div id ='logo'>🎆</div>
+       <div id='intro'> This is my photo gallery </div>
+            </header>
+        `;
+    }
+}
+
+export default Header;
